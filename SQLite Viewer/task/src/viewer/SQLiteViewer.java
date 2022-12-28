b2dd7688b51ac9d03999037682c450a6e68e65f6
KronosGR <geo.elgeo@gmail.com>
2022-12-28 12:15:04 +0100

    stage 2/4 ui update

--- SQLite Viewer/task/src/viewer/SQLiteViewer.java
+++ SQLite Viewer/task/src/viewer/SQLiteViewer.java
@@ -1,6 +1,7 @@
 package viewer;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SQLiteViewer extends JFrame {
 
@@ -13,9 +14,14 @@ public class SQLiteViewer extends JFrame {
         setVisible(true);
         setTitle("SQLite Viewer");
 
+        initializeWindow();
+    }
+
+    private void initializeWindow() {
         JPanel p = new JPanel();
         p.setBounds(10, 10, 680, 50);
         p.setVisible(true);
+        p.setAlignmentY(TOP_ALIGNMENT);
         add(p);
 
         JTextField jt = new JTextField();
@@ -28,5 +34,30 @@ public class SQLiteViewer extends JFrame {
         jb.setName("OpenFileButton");
         jt.setVisible(true);
         p.add(jb);
+
+        JComboBox<String> tablesComboBox = new JComboBox<>();
+        tablesComboBox.setBounds(10, 60, 650, 30);
+        tablesComboBox.setName("TablesComboBox");
+        add(tablesComboBox);
+
+        JPanel p2 = new JPanel();
+        p2.setVisible(true);
+        p2.setLayout(new BoxLayout(p2, BoxLayout.X_AXIS));
+        p2.setMaximumSize(new Dimension(680, 100));
+        p2.setBounds(10, 110, 680, 110);
+        p2.setAlignmentY(Component.TOP_ALIGNMENT);
+        add(p2);
+
+        JTextArea queryTextArea = new JTextArea(8,35);
+        queryTextArea.setName("QueryTextArea ");
+        queryTextArea.setEnabled(false);
+        p2.add(queryTextArea);
+
+
+        JButton executeButton = new JButton("Execute");
+        executeButton.setName("ExecuteQueryButton");
+        executeButton.setEnabled(false);
+        p2.add(executeButton);
+
     }
 }
